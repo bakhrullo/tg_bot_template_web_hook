bd6f1276c69267b082293c63eec4ac6325c1593d
bakhrullo <81313486+bakhrullo@users.noreply.github.com>
2022-12-21 20:13:39 +0500

    first commit

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils.executor import start_webhook
 
 from tgbot.config import load_config
 from tgbot.filters.admin import AdminFilter
@@ -30,7 +31,7 @@ def register_all_handlers(dp):
     register_echo(dp)
 
 
-async def main():
+def main():
     logging.basicConfig(
         level=logging.INFO,
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
@@ -38,9 +39,16 @@ async def main():
     logger.info("Starting bot")
     config = load_config(".env")
 
+    WEBHOOK_PATH = ''
+    WEBHOOK_URL = 'https://your.domain'
+
+    WEBAPP_HOST = 'localhost'  # or ip
+    WEBAPP_PORT = 5000 #if in test use ngrok port
+
     storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
     bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
     dp = Dispatcher(bot, storage=storage)
+    dp.middleware.setup(LoggingMiddleware())
 
     bot['config'] = config
 
@@ -48,17 +56,38 @@ async def main():
     register_all_filters(dp)
     register_all_handlers(dp)
 
-    # start
-    try:
-        await dp.start_polling()
-    finally:
+    async def on_startup(dp):
+        await bot.set_webhook(WEBHOOK_URL)
+        # insert code here to run it after start
+
+    async def on_shutdown(dp):
+        logging.warning('Shutting down..')
+
+        # insert code here to run it before shutdown
+
+        # Remove webhook (not acceptable in some cases)
+        await bot.delete_webhook()
+
+        # Close DB connection (if used)
         await dp.storage.close()
         await dp.storage.wait_closed()
-        await bot.session.close()
+
+        logging.warning('Bye!')
+
+    # start
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
 
 
 if __name__ == '__main__':
     try:
-        asyncio.run(main())
+        main()
     except (KeyboardInterrupt, SystemExit):
         logger.error("Bot stopped!")
